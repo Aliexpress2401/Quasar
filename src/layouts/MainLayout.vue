efe56053976982b293a3982845654f421f38cf99
Aliexpress2401 <alicecrudele2401@gmail.com>
2024-01-16 11:58:42 +0100

    added files

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,142 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+
+      </q-toolbar>
+      <div class="q-px-lg q-px-xl q-mb-md">
+        <div class="text-h3">Todo:</div>
+        <div class="text-subtitle1">{{todaysDate}}</div>
+      </div>
+      <q-img src="src/assets/wallpaper.jpg" class="header-image absolute-top"></q-img>
+    </q-header>
+
+    <q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 144px); margin-top: 144px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+            to="/"
+            exact
+            clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+
+            <q-item
+            to="/help"
+            exact
+            clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+
+
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="src/assets/profile-wall.jpg" style="height: 144px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="src/assets/profile.jpg">
+            </q-avatar>
+            <div class="text-weight-bold">Alice</div>
+            <div>@alicru</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+
+import { date } from 'quasar'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  },
+
+  computed: {
+    todaysDate() {
+      const timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd D MMMM')
+    }
+  }
+
+
+
+
+})
+</script>
+
+<style lang="scss">
+
+.header-image{
+  height: 100%;
+  z-index: -1;
+  opacity: 0.5 white;
+}
+
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>help help help help help help help help help help help help help help help</p>
+    <p>help help help help help help help help help help help help help help help</p>
+    <p>help help help help help help help help help help help help help help help</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelpPage'
+})
+</script>
